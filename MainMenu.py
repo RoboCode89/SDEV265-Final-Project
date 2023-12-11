1b70e94a6ace481c1db2098960cf3f738c4c242f
jgraham135 <120755504+jgraham135@users.noreply.github.com>
2023-12-11 16:16:47 -0500

    Update MainMenu.py

--- MainMenu.py
+++ MainMenu.py
@@ -1,12 +1,13 @@
-
-    
 from FoodMenu import *
 from Menu import *
+from Billing import *
+from Reservations import *
 
-billAmt = []
-billItems = []
+billAmt = [] #Creates list of bills returned from the menuSelection function
+billItems = [] #Creates a list of lists returned from the menuSelection function
+originalBillAmt = [] #I created this list just to help with the billing function
 customerNumber = 1
-maxCustomers = 1
+maxCustomers = 1 #Helps keep track of number of customers and keeping them in sequential order
 customerSelection = 0
 exit = False
 
@@ -22,22 +23,31 @@ while(exit == False):
             print("\n")
             bill, billList = menuSelection()
             billAmt.append(bill)
+            originalBillAmt.append(bill)
+            billItems.append(billList)
         elif(customerNumber <= maxCustomers):
             print("\n")
             bill, billList = menuSelection()
             billAmt.append(bill)
             billItems.append(billList)
+            originalBillAmt.append(bill)
 
 
-    '''if(userInput == 2):
+    if(userInput == 2):
         customerNumber = int(input("Please enter the customer number: "))
         if(customerNumber > maxCustomers):
             print("Invalid customer number entered. Returning to menu.\n")
-        elif(count <= maxCount):
-            billing(billAmt[customerNumber - 1], billItems[customerNumber - 1])
+        elif(customerNumber <= maxCustomers):
+            print("\n")
+            billAmt[customerNumber - 1] = handleBill(billAmt[customerNumber - 1], billItems[customerNumber - 1], originalBillAmt[customerNumber - 1])
+            print("Bill Amount is: ",billAmt[customerNumber - 1])
             
     if(userInput == 3):
-        reservation() '''
+        userInput = int(input("1. Add Reservation \n2. Seat Reservation\n"))
+        if(userInput == 1):
+            add_guest_reservation()#should be activated by button click in the UI
+        if(userInput == 2):
+            seat_guest()#should be activated by button click in the UI
         
     if(userInput == 4):
         finalAnswer = input("Are you sure you want to close? 'y' for yes, 'n' for no: ")
@@ -47,11 +57,3 @@ while(exit == False):
             print("Returning to Menu")
         elif(finalAnswer != "y" or finalAnswer != "n"):
             print("Invalid selection. Returning to menu.\n")
-
-
-'''i = 0
-print("Bill is", "%.2f" %bill)
-print("Items Chosen: ")
-        while(i < len(billItems[0])): #Doing it this way allows to select specific billItems for customer using double array
-            print(billItems[0][i].getName(), " ", billItems[0][i].getPrice())
-            i += 1'''
